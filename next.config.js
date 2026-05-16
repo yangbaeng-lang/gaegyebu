a7ec2842c7af7f932c20acfe7860318d8b7d09dd
yangbaeng-lang <yangbaeng@gmail.com>
2026-05-16 18:06:18 +0900

    fix webpack alias and pin node 20

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require('path')
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
+  }
+}
 module.exports = nextConfig
